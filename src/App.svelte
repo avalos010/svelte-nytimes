ec023be26bd9c597b09af3a35d136e912b8df364
JLuis Avalos <luizavalos40@gmail.com>
2022-05-21 13:38:47 -0500

    Revert "added svelte-navigator"
    This reverts commit 484181f7f48624e50f1f59de0f77aa0246598e66.

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,6 @@
   import { onMount } from "svelte";
   import { getPopularArticles } from "./Api/index";
   import Articles from "./Components/Articles.svelte";
-  import Navigation from "./Components/Navigation.svelte";
-  import { Router, Route, Link } from "svelte-navigator";
 
   export let articles = [];
   onMount(async () => {
@@ -12,24 +10,9 @@
   });
 </script>
 
-<Router>
-  <!-- <main> -->
-  <Navigation />
-  <Route path="/">
-    <h2 class="text-center text-slate-700 text-lg">Most Popular Articles</h2>
-    <Articles {articles} />
-  </Route>
-  <Route path="search">
-    <p>Search articles</p>
-  </Route>
-  <Route>
-    <h3>Default</h3>
-    <p>No Route could be matched.</p>
-    <Link to="home">Home</Link>
-    <Link to="search">Search</Link>
-  </Route>
-  <!-- </main> -->
-</Router>
+<main>
+  <Articles {articles} />
+</main>
 
 <style global>
   @tailwind base;
