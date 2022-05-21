484181f7f48624e50f1f59de0f77aa0246598e66
JLuis Avalos <luizavalos40@gmail.com>
2022-05-21 13:19:28 -0500

    added svelte-navigator

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,8 @@
   import { onMount } from "svelte";
   import { getPopularArticles } from "./Api/index";
   import Articles from "./Components/Articles.svelte";
+  import Navigation from "./Components/Navigation.svelte";
+  import { Router, Route, Link } from "svelte-navigator";
 
   export let articles = [];
   onMount(async () => {
@@ -10,9 +12,24 @@
   });
 </script>
 
-<main>
-  <Articles {articles} />
-</main>
+<Router>
+  <!-- <main> -->
+  <Navigation />
+  <Route path="/">
+    <h2 class="text-center text-slate-700 text-lg">Most Popular Articles</h2>
+    <Articles {articles} />
+  </Route>
+  <Route path="search">
+    <p>Search articles</p>
+  </Route>
+  <Route>
+    <h3>Default</h3>
+    <p>No Route could be matched.</p>
+    <Link to="home">Home</Link>
+    <Link to="search">Search</Link>
+  </Route>
+  <!-- </main> -->
+</Router>
 
 <style global>
   @tailwind base;
--- src/Components/Articles.svelte
+++ src/Components/Articles.svelte
@@ -1,10 +1,13 @@
 <script>
   import Article from "./Article.svelte";
+
   export let articles;
 </script>
 
 <div class="flex flex-col flex-wrap justify-center items-center">
+  <!-- {console.log(articles)} -->
   {#each articles as article}
     <Article {article} />
+    <p>{article.uri}</p>
   {/each}
 </div>
